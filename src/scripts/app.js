3d550d2d7814da1531b629bade020d2209db86c2
ZoroAi <sauravkc456@gmail.com>
2023-01-25 15:33:25 +0000

    a better nav bar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,5 @@
-import { hello } from "./tests.mjs";    
+import { hello } from "./tests.mjs";   
+import { openCloseMenu, openCloseSearch } from "./functions/navSearch.mjs" 
 hello();
+
+openCloseSearch(); openCloseMenu();
