973c15e4bda182caa5370a27b8f2fbfebb5cea34
ZoroAi <sauravkc456@gmail.com>
2023-02-04 16:17:48 +0000

    .

--- src/models.py
+++ src/models.py
@@ -4,16 +4,21 @@ from sqlalchemy.sql import func
 
 # Database Models
 class User(db.Model, UserMixin):
-    # User info to store
+    # User info
     id = db.Column(db.Integer, primary_key=True)
-    notes = db.relationship('Product')
+    notes = db.relationship('Basket')
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
     
 class Product(db.Model):
+    # Product info
     id = db.Column(db.Integer, primary_key = True)
     basket = db.Column(db.Integer, db.ForeignKey('user.id'))
     p_name = db.Column(db.String(150))
     p_price = db.Column(db.Float(1000))
     
+class Basket(db.Model):
+    # Basket info
+    id = db.Column(db.Integer, primary_key = True)
+    product = db.relationship('Product')
