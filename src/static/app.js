4d515130d5be83d4022655da9aaf212f3c40d92c
ZoroAi <sauravkc456@gmail.com>
2023-02-20 19:02:37 +0000

    js attempt

--- src/static/app.js
+++ src/static/app.js
@@ -1,15 +1,32 @@
 import { hello, showItems } from "./scripts/tests.mjs";    
+import {} from "./scripts/functions/search.mjs";
 
 
 hello();
 
 
-searchBar = document.getElementById('searchBar');
-searchBar.addEventListener('input', (e) => {
-    let value = e.target.value
-    if (value > 0){
+// searchBar = document.getElementById('searchBar');
+// searchBar.addEventListener('input', (e) => {
+//     let value = e.target.value;
+//     // Check if input exists and is greater than 0
+//     if (value && value.trim().length > 0){
+//         value. value.trim().toLowerCase();
 
-    }else{
+//         // Return results that include the value of the search
+//         setDiv(items.filter(item => {
+//             return item.name.includes(value);
+//         }))
+//     }else{
         
+//     }
+// })
+
+function setDiv(results){
+    for(const item of results){
+        const resultItem= document.createElement('div');
+        resultItem.classList.add('result-item');
+        const text = document.createTextNode(item.title)
+        resultItem.appendChild(text)
+        list.appendChild(resultItem)
     }
-})
+}
--- src/static/scripts/functions/search.mjs
+++ src/static/scripts/functions/search.mjs
@@ -1,1 +1,9 @@
-
+// export function setDiv(results){
+//     for(const item of results){
+//         const resultItem= document.createElement('div');
+//         resultItem.classList.add('result-item');
+//         const text = document.createTextNode(item.title)
+//         resultItem.appendChild(text)
+//         list.appendChild(resultItem)
+//     }
+// }
