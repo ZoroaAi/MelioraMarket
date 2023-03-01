56f3eb4441fd2ac473737fc749c544cf0b2fbf95
ZoroAi <sauravkc456@gmail.com>
2023-03-01 18:21:53 +0000

    Tried adding weight info to json but failed

--- src/market_scrapers/modularised_apporach.py
+++ src/market_scrapers/modularised_apporach.py
@@ -5,13 +5,15 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
+import re
 
 class Scraper:
-    def __init__(self, base_url: str, page_number: int, product_container_locator: str, product_name: str,product_price: str,product_image: str,wait_time: int = 5):
+    def __init__(self,driver: webdriver.Chrome, base_url: str, page_number: int, product_container_locator: str, product_title: str,product_price: str,product_image: str,wait_time: int = 5):
+        self.driver = driver
         self.base_url = base_url
         self.page_number = page_number
         self.product_container_locator = product_container_locator
-        self.product_title = product_name
+        self.product_title = product_title
         self.product_price = product_price
         self.product_image = product_image
         self.wait_time = wait_time
@@ -19,27 +21,55 @@ class Scraper:
         self.data = []
 
     def scrape(self):
+        # the main function for getting data and appending to dictionary
         for page in range(1, self.page_number + 1):
             url = f"{self.base_url}{page}"
             self.driver.get(url)
             product_container = self.driver.find_elements(By.XPATH, self.product_container_locator)
             products = []
             for product in product_container:
-                title = self.get_product_title(product)
+                title, weight = self.get_product_title(product)
+                title = self.clean_product_title(title)
+                print("Cleaned: " + title)
                 img_url = self.get_product_image_url(product)
                 price = self.get_product_price(product)
                 product_data = {
                     "title": title,
                     "img_url": img_url,
                     "price": price,
+                    "weight": weight
                 }
                 products.append(product_data)
             self.data.extend(products)
 
+    # def clean_product_title(self, title: str) -> str:
+    #     title = title.replace('Tesco', '').strip()
+    #     weight_idx = title.rfind(' ')
+    #     if weight_idx != -1:
+    #         title = title[:weight_idx]
+    #     return title
+    
+    def get_product_weight(self, title: str) -> str:
+        title = title.strip()
+        weight_idx = title.rfind(' ')
+        if weight_idx != -1:
+            weight = title[weight_idx+1:].strip().rstrip('0123456789 .')
+            return weight
+        return None
+    
+    def clean_product_title(self, title: str) -> str:
+        title = re.sub(r'Tesco\s+','',title)
+        title = re.sub(r'\d+G\s*\+*','',title)
+        return title.strip()
+
     def get_product_title(self, product: WebElement) -> str:
         title_locator = (By.XPATH, self.product_title)
-        title = self.wait.until(EC.visibility_of_element_located(title_locator))
-        return title.text
+        title_element = self.wait.until(EC.visibility_of_element_located(title_locator))
+        print("Unclean: " + title_element.text)
+        title = title_element.text.strip()
+        weight = self.get_product_weight(title)
+        title = self.clean_product_title(title)
+        return title, weight
 
     def get_product_image_url(self, product: WebElement) -> str:
         image_locator = (By.XPATH, self.product_image)
@@ -50,13 +80,15 @@ class Scraper:
         price_locator = (By.XPATH, self.product_price)
         try:
             price = self.wait.until(EC.visibility_of_element_located(price_locator))
+            price = price.text
         except:
             price = "Out of Stock"
-        return price.text
+        return price.replace("u00a3", "£")
 
     def write_to_file(self, file_path: str):
         with open(file_path, 'w') as file:
             json.dump(self.data, file)
+        
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
@@ -65,13 +97,14 @@ if __name__ == '__main__':
         xpath_data = json.load(f)
     
     tesco_scraper = Scraper(
+        driver = driver,
         base_url = xpath_data['tesco']['base_url'],
-        page_number = 2,
+        page_number = 1,
         product_container_locator = xpath_data['tesco']['product_container'],
-        product_title = xpath_data['tesco']['product_name'],
+        product_title = xpath_data['tesco']['product_title'],
         product_price = xpath_data['tesco']['product_price'],
         product_image = xpath_data['tesco']['product_image']
     )
     tesco_scraper.scrape()
-    tesco_scraper.write_to_file('src/scraped_data/tesco_data.json')
+    tesco_scraper.write_to_file('src/scraped_data/tesco_data7.json')
     driver.quit()
